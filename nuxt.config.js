608c98b3d89a952f1c176b7d946b2b1ea6d1961c
Louistiti <louis.grenard@gmail.com>
2019-02-05 20:49:52 +0800

    style: add dots to meta descriptions

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,14 +13,14 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'theme-color', content: '#1c75bd' },
-      { hid: 'description', name: 'description', content: 'Leon is an open-source personal assistant who can live on your server. He is built on the top of Node.js, Python and artificial intelligence concepts' },
+      { hid: 'description', name: 'description', content: 'Leon is an open-source personal assistant who can live on your server. He is built on the top of Node.js, Python and artificial intelligence concepts.' },
       { name: 'og:type', content: 'website' },
       { name: 'og:title', content: 'Leon - Your Open-Source Personal Assistant' },
-      { name: 'og:description', content: 'Leon is an open-source personal assistant who can live on your server. He is built on the top of Node.js, Python and artificial intelligence concepts' },
+      { name: 'og:description', content: 'Leon is an open-source personal assistant who can live on your server. He is built on the top of Node.js, Python and artificial intelligence concepts.' },
       { name: 'og:image', content: 'https://getleon.ai/img/logo_t.png' },
       { name: 'twitter:card', content: 'summary' },
       { name: 'twitter:title', content: 'Leon - Your Open-Source Personal Assistant' },
-      { name: 'twitter:description', content: 'Leon is an open-source personal assistant who can live on your server. He is built on the top of Node.js, Python and artificial intelligence concepts' },
+      { name: 'twitter:description', content: 'Leon is an open-source personal assistant who can live on your server. He is built on the top of Node.js, Python and artificial intelligence concepts.' },
       { name: 'twitter:image', content: 'https://getleon.ai/img/logo_t.png' }
     ],
     link: [
