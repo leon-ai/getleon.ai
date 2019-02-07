c5ca6e6ad3537d5d24cbf8e136ca9dfdbbbaa0d7
Louistiti <louis.grenard@gmail.com>
2019-02-08 01:26:10 +0800

    style: edit curve shadow

--- components/the-footer.vue
+++ components/the-footer.vue
@@ -1,12 +1,12 @@
 <template>
   <section>
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1398 118">
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1398 91">
       <defs>
         <path id="curve-b" d="M-19,0 C77.2479193,54.236685 213.716148,81.3550275 390.404686,81.3550275 C674.819327,81.3550275 698.034242,11.2133249 997.586563,6.51972235 C1091.45274,5.04895923 1290.92929,12.3062895 1392.76355,50.1324221 C1431.02506,64.3445824 1495.10388,82.9671084 1585,106 L1585,0 L-19,0 Z"/>
         <filter id="curve-a" width="101.3%" height="119.8%" x="-.7%" y="-7.1%" filterUnits="objectBoundingBox">
           <feOffset dy="3" in="SourceAlpha" result="shadowOffsetOuter1"/>
           <feGaussianBlur in="shadowOffsetOuter1" result="shadowBlurOuter1" stdDeviation="3"/>
-          <feColorMatrix in="shadowBlurOuter1" values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.25 0"/>
+          <feColorMatrix in="shadowBlurOuter1" values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.16 0"/>
         </filter>
       </defs>
       <g fill="none" fill-rule="evenodd">
