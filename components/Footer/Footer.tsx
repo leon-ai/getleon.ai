63630a51539aab5e8af753e7837f2d23de976998
louistiti <louis.grenard@gmail.com>
2021-12-05 21:21:50 +0800

    style(footer): add links

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -58,6 +58,7 @@ const Footer: React.FC<IFooterProps> = () => {
                 Stay Tuned
               </span>
               <ul>
+                <li><a href="https://twitter.com/louistiti_fr" target="_blank" rel="noreferrer">Twitter</a></li>
                 <li><a href="https://blog.getleon.ai" target="_blank" rel="noreferrer">Blog</a></li>
                 <li><a href="https://roadmap.getleon.ai" target="_blank" rel="noreferrer">Roadmap</a></li>
                 <li><a href="https://changelog.getleon.ai" target="_blank" rel="noreferrer">Changelog</a></li>
@@ -78,6 +79,7 @@ const Footer: React.FC<IFooterProps> = () => {
               </span>
               <ul>
                 <li><a href="https://github.com/leon-ai/leon" target="_blank" rel="noreferrer">GitHub</a></li>
+                <li><a href="https://www.youtube.com/channel/UCW6mk6j6nQUzFYY97r47emQ" target="_blank" rel="noreferrer">YouTube</a></li>
                 <li><a href="https://github.com/leon-ai/leon/blob/develop/.github/CONTRIBUTING.md" target="_blank" rel="noreferrer">Contribute</a></li>
                 <li><a href="https://github.com/leon-ai/leon/issues" target="_blank" rel="noreferrer">Help</a></li>
                 <li><a href="https://sponsor.getleon.ai" target="_blank" rel="noreferrer">Become a sponsor</a></li>
