c7a45fce1f934a56c971f8f4b8ae9cba5df030b7
louistiti <louis.grenard@gmail.com>
2022-10-19 23:28:56 +0800

    feat: add JSON highlight on Pastebin report

--- pages/api/report.ts
+++ pages/api/report.ts
@@ -18,12 +18,25 @@ export default async function handler (
   try {
     let { report } = req.body
 
-    report = JSON.stringify(report)
+    report = JSON.stringify({ report }, null, 2)
 
+    /**
+     * Pastebin docs
+     * @see https://pastebin.com/doc_api
+     */
     const pasteName = '[Leon] Report - ' + new Date().toISOString()
+    const params = [
+      `api_dev_key=${PASTEBIN_API_DEV_KEY}`,
+      `api_paste_name=${pasteName}`,
+      `api_paste_code=${report}`,
+      `api_user_key=${PASTEBIN_API_USER_KEY}`,
+      'api_paste_format=json',
+      'api_option=paste',
+      'api_paste_private=0' // Public
+    ]
     const response = await fetch('https://pastebin.com/api/api_post.php', {
       method: 'POST',
-      body: `api_dev_key=${PASTEBIN_API_DEV_KEY}&api_paste_name=${pasteName}&api_paste_code=${report}&api_option=paste&api_paste_private=1&api_user_key=${PASTEBIN_API_USER_KEY}`,
+      body: params.join('&'),
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       }
